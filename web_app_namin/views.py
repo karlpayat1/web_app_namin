4aeac509e89ea1b15dc62c04978d9f141e51e3f5
karlpayat1 <zlerf52@gmail.com>
2018-08-31 04:53:49 +0800

    bleh

--- web_app_namin/views.py
+++ web_app_namin/views.py
@@ -12,3 +12,15 @@ def my_view(request):
 	if 'blink' in request.params:
 		os.system("python3 /home/pi/Desktop/blink.py")
 	return {'project': 'web-app-namin'}
+
+def app_users(request):
+	finame=str(request.POST.get('firstname'))
+	laname=str(request.POST.get('lastname'))
+	dcrption=str(request.POST.get('description'))
+	uname=str(request.POST.get('username'))
+	pnumber=str(request.POST.get('phonenumber'))
+	if AppUsers.objects(username=uname).first():
+		return{"error": "USERNAME ALREADY EXISTS"}
+	x=AppUsers(firstname=finame,lastname=laname,description=dcrption,username=uname,phonenumber=pnumber)
+	x.save()
+	return{"response": "DATA ADDED"}
